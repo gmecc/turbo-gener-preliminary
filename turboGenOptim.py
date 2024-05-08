20984253132c4a026f502644feca9e2db5f3362c
SergeyMtt <sb68595@gmail.com>
2024-05-08 07:28:32 +0200

    arnold

--- turboGenOptim.py
+++ turboGenOptim.py
@@ -28,7 +28,6 @@ class TurboGenOptim:
         self.T_in = T_in
         self.pit = pit
         self.P_out = P_out 
-        
         self.R = cst.R / prop.mol[self.fluid]*1000 # удельная газовая постоянная (Дж/кг*К)
         self.k = prop.k[self.fluid] # показатель адиабаты
         self.Cp = self.k / (self.k - 1) * self.R # удельная изобарная теплоемкость (Дж/кг*К)
@@ -36,111 +35,103 @@ class TurboGenOptim:
         self.dh1s = self.Cp * self.T_in * (1 - 1 / (self.pit**((self.k-1)/self.k))) 
         
     def rotation_frequency_opt(self, pwr):
-        self.pwr = pwr 
         self.efc = .87
         self.dh1 = self.dh1s * self.efc
         self.T_out = (self.h0 - self.dh1)/ self.Cp
-        self.G = self.pwr / self.dh1 
+        self.G = pwr / self.dh1 
         self.V2 = self.R * self.T_out / self.P_out 
         self.Gv = self.G * self.V2 
-        self.n_opt = 0.548 * (self.dh1)**(3/4) / ( math.pi * (self.Gv**0.5))*60
+        self.n_opt = 0.548 * (self.dh1s)**(3/4) / ( 2 * math.pi * (self.Gv**0.5))
         return self.n_opt
-        
-        
-        
-    def specific_speed(self, pwr, n=1000, efc=.86):
-        self.efc = efc
-        
-        
-
-    def _solve(self, pwr, n=1000, efc=.86):
-        # self.pwr = pwr
-        self.n = n
-        self.dh1 = self.dh1s * efc
-        self.T_out = (self.h0 - self.dh1s)/ self.Cp
-        self.G = self.pwr / self.dh1 
-        self.V2 = self.R * self.T_out / self.P_out / 100
-        self.Gv = self.G * self.V2 
-        self.mfp = self.G * self.T_in**0.5 / self.P_in * 10
-        self.Ns = 2 * math.pi * n * (self.Gv**0.5) / (self.dh1*1000)**(3/4) 
-        self.n_opt = 0.5 * (self.dh1*1000)**(3/4) / (2 * math.pi * (self.Gv**0.5))
-        # self.n_opt = self.n_opts * 60 
-        self.efcs = 0.87 - 1.07 * (self.Ns-0.55)**2 - 0.5 * (self.Ns-0.55)**3
-        ds.efc = self.efc
-        ds.pwr = self.pwr
-        ds.n = self.n * 60
-        ds.dh1 = self.dh1
-        ds.T_out = self.T_out
-        ds.G = self.G
-        ds.V2 = self.V2
-        ds.Gv = self.Gv
-        ds.mfp = self.mfp
-        ds.Ns = self.Ns
-        ds.efcs = self.efcs
-        ds.n_opt = self.n_opt       
-
-    def power(self, pwr): # оптимизация при n_opt
-        self.pwr = pwr
-        self.n = 100
-        self._solve(efc=0.8, pwr=self.pwr, n=self.n)
-        self.ef = 0.6
-        while abs(self.ef - self.efcs) > 0.002:
-            self.ef += 0.001
-            self._solve(efc=self.ef, pwr=self.pwr, n=self.n_opt)
-        ds.n_optm = self.n_opt * 60  
-        ds.efcp = self.efcs
-        return self.n_opt * 60
-
-    def powernset(self, pwr, ns): # оптимизация при заданной n
-        self.pwr = pwr
-        self.ns = ns
-        self.n = 100
-        self._solve(efc=0.8, pwr=self.pwr, n=self.n)
-        self.ef = 0.6
-        while abs(self.ef - self.efcs) > 0.002:
-            self.ef += 0.001
-            self._solve(efc=self.ef, pwr=self.pwr, n=self.ns)
     
-    def powerplot(self):
-        ng = 50
-        nsi = np.zeros(ng)
-        pwri = np.linspace(10, 200, ng)
-        for i in range(ng):
-            self.power(pwri[i])
-            nsi[i] = self.n_opt * 60 / 1000
+    def arnold(self, n, pwr, u_max, k_ld=1.8):
+        self.alpha_d = 0.64
+        self.k_f = 1.11
+        self.k_0 = 0.92
+        self.A = 3e4
+        self.B_delta = 0.45
+        self.k_e = 1.2
+        self.u_max = u_max
+        self.d_rot = u_max / (math.pi * n)
+        self.k_ld = k_ld
+        self.l = self.d_rot * self.k_ld
+        self.CA_param = 2 / (math.pi * self.alpha_d * self.k_f * self.k_0 * self.A * self.B_delta)
+        self.CA = self.d_rot**2 * self.l * n / (self.k_e * pwr)
+        self.k_arnold = self.CA / self.CA_param
+        return self.CA      
+    
+    def turbogenerator(self, pwr, efc_gen=.95): # расчет постоянной Арнольда
+        self.pwr = pwr 
+        pwr_turbine = pwr / efc_gen
+        koeff_pow = .98
+        self.rotation_frequency_opt(pwr_turbine)
+        print(f'Частота вращения оптимальная {self.n_opt*60:.0f}')
+        self.arnold(n=self.n_opt, pwr=pwr, u_max=250) 
+        print(f'Постоянная Арнольда {self.CA:.3e}')
+        print(f'Коэффициент постоянной Арнольда {self.k_arnold:.3f}')
+
+    def powergen(self, n):
+        d_rot = self.u_max / (math.pi * n)
+        l = d_rot * self.k_ld
+        Pn = d_rot ** 2 * l * n / (self.k_e * self.CA)
+        return Pn
+
+    def plot(self, pwr):
+        pwr_min, pwr_max = pwr
+        pwri = np.linspace(pwr_min, pwr_max)
+        n_opt = self.rotation_frequency_opt(pwri)
+        plt.plot(n_opt/1000*60, pwri/1000, c='red', label='turbine')
         
-        # ПОСТРОЕНИЕ ГРАФИКА
-        plt.plot(nsi, pwri, c='red', label='turbine')
+        Pn = self.powergen(n=n_opt)
+        plt.plot(n_opt*60/1000, Pn/1000, c='blue', label='generator')
+        
+        plt.hlines(self.pwr/1000, n_opt[0]/1000*60, n_opt[-1]/1000*60, linewidth=0.5, linestyle='--', color='black')
+        
         plt.xlabel('Частота вращения, т.об/мин', fontsize= 10) 
         plt.ylabel('Мощность, кВт', fontsize= 10) 
-        # plt.legend()
+        plt.legend()
         plt.grid(linestyle='--', linewidth=0.5)
-        plt.title(self.name, fontsize= 10 , loc='left')
-        # plt.show()
-
-class gener:
-    def __init__(self):
-        alpha_d = 0.64
-        k_f = 1.11
-        k_0 = 0.92
-        A = 3e4
-        B_delta = 0.45
-        self.k_e = 1.2
-        self.CA = 2 / (math.pi * alpha_d * k_f * k_0 * A * B_delta)
-    def povergen(self, u_max, k_ld):
-        self.k_ld = k_ld
-        self.u_max = u_max
-        d_rot = np.arange(0.04, 0.18, 0.01)
-        n = u_max / (math.pi * d_rot) 
-        l = d_rot * k_ld
-        Pn = 2 * math.pi * d_rot ** 2 * l * n / (self.k_e * self.CA)
-        return n, Pn, d_rot        
+        plt.savefig('turbine-optim.png', dpi = 300)
+        plt.show()
 
 
+    # def generator(self, u_max, k_ld=1.5):
+    #     alpha_d = 0.64
+    #     k_f = 1.11
+    #     k_0 = 0.92
+    #     A = 3e4
+    #     B_delta = 0.45
+    #     self.k_e = 1.2
+    #     self.CA = 2 / (math.pi * alpha_d * k_f * k_0 * A * B_delta)
+        
+    #     self.k_ld = k_ld
+    #     self.u_max = u_max
+    #     d_rot = np.arange(0.04, 0.18, 0.01)
+    #     n = u_max / (math.pi * d_rot) 
+    #     l = d_rot * k_ld
+    #     Pn = 2 * math.pi * d_rot ** 2 * l * n / (self.k_e * self.CA)
+    #     return n, Pn, d_rot        
 
-tu = TurboGenOptim(pit=3, T_in=950) # создание экземпляра класса turbo
-tu.rotation_frequency_opt(pwr=1e5)
-print(f'{tu.n_opt:.0f}')
+        
+    
+
+
+
+
+tu = TurboGenOptim(pit=3, T_in=1250) # создание экземпляра класса turbo
+# tu.rotation_frequency_opt(pwr=1e5)
+# print(f'{tu.n_opt:.0f}')
+
+tu.turbogenerator(pwr=1e5)
+tu.plot(pwr=(20000, 200000))
+
+
+
+
+
+
+
+
 
 
 
