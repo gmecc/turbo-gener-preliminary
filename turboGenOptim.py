266be109634eb68226b8747c377b2270f9e96fe4
SergeyMtt <sb68595@gmail.com>
2024-05-09 06:25:22 +0200

    read dounload

--- turboGenOptim.py
+++ turboGenOptim.py
@@ -12,9 +12,9 @@ import math
 import matplotlib.pyplot as plt 
 
 gas = pd.DataFrame({
-    'mol': [28.965, 16.043, 0, 0],
-    'k': [1.4017, 1.3053, 0, 0]},
-    index=['air', 'methane', 'nitrogen', 'helium'],
+    'mol': [28.965, 16.043],
+    'k': [1.4017, 1.3053]},
+    index=['air', 'methane'],
     dtype=float)
 
 class TurboGenOptim:
@@ -39,39 +39,36 @@ class TurboGenOptim:
         self.n_opt = 0.548 * (self.dh1s)**(3/4) / ( 2 * math.pi * (self.Gv**0.5))
         return self.n_opt
     
-    def arnold(self, n, pwr, u_max, k_ld=2):
-        self.alpha_d = 0.64
+    def arnold(self, n, pwr, u_max, k_ld=1.5):
+        self.alpha_i = 0.64
         self.k_f = 1.11
         self.k_0 = 0.92
-        self.A = 3e5
+        self.A = 200
         self.B_delta = 0.45
         self.k_e = 1.2
         self.u_max = u_max
         self.d_rot = u_max / (math.pi * n)
         self.k_ld = k_ld
         self.l = self.d_rot * self.k_ld
-        self.CA_regular = 2 / (math.pi * self.alpha_d * self.k_f * self.k_0 * self.A * self.B_delta)
-        self.CA = self.d_rot**2 * self.l * n / (self.k_e * pwr)
-        self.k_arnold = math.log10(self.CA / self.CA_regular) * 10
+        self.CA_regular = 6.1e7 / ( self.alpha_i * self.k_f * self.k_0 * self.A * self.B_delta)
+        self.CA = (self.d_rot * 100)**2 * (self.l * 100) * (n * 60) / (self.k_e * pwr / 1000)
+        self.k_arnold = math.log10(self.CA_regular / self.CA) * 10
         return self.CA      
     
     def turbogenerator(self, pwr, efc_gen=.95): # расчет постоянной Арнольда
         self.pwr = pwr 
         pwr_turbine = pwr / efc_gen
         koeff_pow = .98
-        self.rotation_frequency_opt(pwr_turbine)
-        print(f'Частота вращения оптимальная {self.n_opt*60:.0f}')
-        self.arnold(n=self.n_opt, pwr=pwr, u_max=250) 
-        print(f'Постоянная Арнольда {self.CA:.3e}')
-        print(f'Постоянная Арнольда {self.k_arnold:.3f} dB')
+        self.n_opt_turbo = self.rotation_frequency_opt(pwr_turbine)
+        self.arnold(n=self.n_opt, pwr=pwr, u_max=120) 
 
     def powergen(self, n, CA):
         d_rot = self.u_max / (math.pi * n)
         l = d_rot * self.k_ld
-        Pn = d_rot ** 2 * l * n / (self.k_e * CA)
+        Pn = (d_rot * 100) ** 2 * (l * 100) * (n * 60) / (self.k_e * CA ) * 1000 
         return Pn
 
-    def plot(self, pwr, generator_regular=False):
+    def plot(self, pwr):
         pwr_min, pwr_max = pwr
         pwri = np.linspace(pwr_min, pwr_max)
         n_opt = self.rotation_frequency_opt(pwri)
@@ -80,24 +77,24 @@ class TurboGenOptim:
         Pn = self.powergen(n=n_opt, CA=self.CA)
         plt.plot(n_opt*60/1000, Pn/1000, c='blue', label='generator')
         
-        if generator_regular != False:
-            Pn = self.powergen(n=n_opt, CA=self.CA_regular)
-            plt.plot(n_opt*60/1000, Pn/1000, c='green', label='generator_regular', linestyle='--')
-        
         plt.hlines(self.pwr/1000, n_opt[0]/1000*60, n_opt[-1]/1000*60, linewidth=0.5, linestyle='--', color='black')
+        plt.vlines(self.n_opt_turbo/1000*60, pwri[0]/1000, pwri[-1]/1000, linewidth=0.5, linestyle='--', color='black')
         
         plt.xlabel('Частота вращения, т.об/мин', fontsize= 10) 
         plt.ylabel('Мощность, кВт', fontsize= 10) 
         plt.legend()
         plt.grid(linestyle='--', linewidth=0.5)
+        plt.minorticks_on()
         plt.savefig('turbine-optim.png', dpi = 300)
         plt.show()
 
 
-tu = TurboGenOptim(pit=3, T_in=1250) # создание экземпляра класса turbo
-tu.turbogenerator(pwr=1e5)
-tu.plot(pwr=(20000, 200000), generator_regular=True)
+# tu = TurboGenOptim(pit=3, T_in=1250) # создание экземпляра класса turbo
+# tu.turbogenerator(pwr=1e5)
+# tu.plot(pwr=(20000, 200000))
 
+# print(f'Частота вращения турбины оптимальная {tu.n_opt_turbo*60:.0f} об/мин')
+# print(f'Постоянная Арнольда {tu.k_arnold:.3f} dB')
 
 
 
